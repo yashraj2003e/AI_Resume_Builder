e1ef3015b9e36136cc1e44ef2b9d86a957cf5dfb
Yashraj <yashsharmau9@gmail.com>
2024-08-22 23:57:05 +0530

    Modified save feature for skills section

--- src/components/custom/Form/Skills.jsx
+++ src/components/custom/Form/Skills.jsx
@@ -1,44 +1,63 @@
-import PropTypes from "prop-types";
-import SkillItem from "./SkillsItem";
+import proptypes from "prop-types";
 import { useEffect, useState } from "react";
 import { Input } from "../../ui/input";
 import Star from "../StarRating/Star";
 import { Button } from "../../ui/button";
 import { useResumeContext } from "../../../contexts/ResumeContext";
-
-const data2 = [
-  {
-    id: 0,
-    name: "",
-    proficiency: 0,
-  },
-];
-
-const emptyData = {
-  id: 0,
-  name: "",
-  proficiency: 0,
-};
+import GlobalAPI from "../../../../service/GlobalAPI";
+import { useParams } from "react-router-dom";
 
 function getNewArray(resumeInfo) {
-  let newData = [...resumeInfo.skills];
-  newData = newData.map((obj) => ({ ...obj, rating: obj.rating / 20 }));
+  let newData = resumeInfo;
+  newData = newData.map((obj) => ({ ...obj, rating: obj.rating * 20 }));
   return newData;
 }
 
-function Skills() {
+Skills.propTypes = {
+  setIsLoading: proptypes.func.isRequired,
+};
+
+function Skills({ setIsLoading }) {
+  const { resumeId } = useParams();
   const { resumeInfo, setResumeInfo } = useResumeContext();
-  const [totalSkills, setTotalSkills] = useState(
-    resumeInfo.skills.length > 0 ? resumeInfo.skills.length : 1
-  );
-  const [data, setData] = useState(
-    resumeInfo.skills.length > 0 ? getNewArray(resumeInfo) : []
-  );
+  const [totalSkills, setTotalSkills] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => setTotalSkills(data.length), [data.length]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        if (data.length === 0 && resumeInfo.skills.length === 0) {
+          console.log("fetching skills data");
+          setIsLoading(true);
+          const data1 = await GlobalAPI.getUserResumeData(resumeId);
+          const result = data1.data.data;
+          if (result[0].userSkills.length > 0) {
+            setData(result[0].userSkills);
+            setResumeInfo((prev) => ({
+              ...prev,
+              skills: getNewArray(result[0].userSkills),
+            }));
+          }
+        } else if (data.length === 0) {
+          console.log("setting from resumeInfo Skills");
+          console.log(resumeInfo.skills);
+          setData(resumeInfo.skills);
+        }
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [data, resumeId, resumeInfo.skills, setIsLoading, setResumeInfo]);
 
   useEffect(() => {
     if (data.length === 0 && resumeInfo.skills.length > 0) {
       let newData = [...resumeInfo.skills];
-      newData = newData.map((obj) => ({ ...obj, rating: obj.rating / 20 }));
+      newData = newData.map((obj) => ({ ...obj, rating: obj.rating }));
       console.log(newData);
       setData(newData);
       setTotalSkills(resumeInfo.skills.length);
@@ -47,8 +66,26 @@ function Skills() {
 
   function handleSave(e) {
     e.preventDefault();
-    const newData = data.map((obj) => ({ ...obj, rating: obj.rating * 20 }));
-    setResumeInfo((resume) => ({ ...resume, skills: newData }));
+    const newData = data.map((obj) => ({ ...obj, rating: obj.rating }));
+    setResumeInfo((resume) => ({ ...resume, skills: getNewArray(newData) }));
+    async function saveData() {
+      try {
+        console.log("Saving Data");
+        setIsLoading(true);
+        const data = {
+          data: {
+            userSkills: JSON.stringify(newData),
+          },
+        };
+        const updatePromise = GlobalAPI.UpdateResumeDetails(resumeId, data);
+        await updatePromise;
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    saveData();
   }
 
   return (
@@ -98,6 +135,7 @@ function Skills() {
                 variant="outline"
                 className="border-primary text-primary"
                 onClick={() => setTotalSkills((prev) => prev + 1)}
+                type="button"
               >
                 + Add More
               </Button>
